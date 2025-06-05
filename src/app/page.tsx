d52b66b26dee0c61ce469b3160151d2d8c5f8eb9
YuWonKyu <duck01777@naver.com>
2025-06-06 02:51:31 +0900

    fix: 임시추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import MyProfile from "@/app/myprofile/page";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold text-main">홈 페이지입니다 🎉</h1>
+    <main className="flex flex-col min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-black pb-50">
+        홈 페이지입니다 🎉
+      </h1>
       <MyProfile />
     </main>
   );
--- src/components/button/commonButton.tsx
+++ src/components/button/commonButton.tsx
@@ -25,21 +25,21 @@ const CommonButton = ({
 }: CommonButtonProps) => {
   const hasIcon = variant === "social-google" || variant === "social-kakao";
   const baseStyle =
-    "flex items-center justify-center text-center cursor-pointer ";
+    "flex items-center justify-center text-center cursor-pointer whitespace-nowrap tablet:whitespace-nowrap";
 
   const variantStyles: Record<ButtonVariant, string> = {
     "social-google":
-      "w-303 h-48 gap-[10px] py-14 rounded-[12px] text-md text-gray-800 border border-gray-300 bg-white font-medium tablet:w-400 tablet:h-52 tablet:gap-12 tablet:text-lg",
+      "w-303 h-48 gap-10 px-81.5 py-14 rounded-[12px] text-md text-gray-800 border border-gray-300 bg-white font-medium tablet:w-400 tablet:h-52 tablet:gap-12 tablet:text-lg",
     "social-kakao":
-      "w-303 h-48 gap-[10px] py-14 rounded-[12px] text-md text-gray-800 border border-gray-300 bg-white font-medium tablet:w-400 tablet:h-52 tablet:gap-12 tablet:text-lg",
+      "w-303 h-48 gap-10 py-14 px-81.5 rounded-[12px] text-md text-gray-800 border border-gray-300 bg-white font-medium tablet:w-400 tablet:h-52 tablet:gap-12 tablet:text-lg",
     "modal-cancel":
       "w-61 h-40 px-18 py-16 rounded-[12px] text-md text-gray-500 border border-gray-300 bg-white font-medium tablet:w-68 tablet:h-42 tablet:px-20 tablet:text-lg",
     "modal-submit":
-      "w-100 h-40 px-18 py-16 rounded-[12px] text-md text-white bg-primary-purple font-bold border-none tablet:w-113 tablet:h-42 tablet:px-20 tablet:text-lg",
+      "w-100 h-40 px-18 py-16 rounded-[12px] text-md text-white bg-main font-bold border-none tablet:w-113 tablet:h-42 tablet:px-20 tablet:text-lg ",
     signup:
-      "w-343 h-48 px-172 py-16 rounded-[12px] text-md text-white bg-primary-purple font-bold tablet:w-400 tablet:h-50 tablet:rounded-[16px] tablet:text-lg",
+      "w-343 h-48 px-172 py-16 rounded-[12px] text-md text-white bg-main font-bold tablet:w-400 tablet:h-50 tablet:rounded-[16px] tablet:text-lg",
     review:
-      "w-169 h-48 px-48 py-16 rounded-[12px] text-white bg-primary-purple font-semibold text-lg",
+      "w-169 h-48 px-48 py-16 rounded-[12px] text-white bg-main font-semibold text-lg",
   };
 
   return (
