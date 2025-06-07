71e52a724b3565572f1692e21e6ee51c0998ccce
wonkyu <duck01777@naver.com>
2025-06-07 13:41:21 +0900

    feat: 앱라우터 폴더구조 생성

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import MyProfile from "@/app/myprofile/page";
+
+export default function HomePage() {
+  return (
+    <main >
+      <h1 className="text-3xl font-bold text-black pb-50">
+        랜딩 페이지입니다 🎉
+      </h1>
+      <MyProfile />
+    </main>
+  );
+}
