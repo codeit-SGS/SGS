ccb7c4d6422c85c069681e18e41ac2b7a0e78c1a
YuWonKyu <duck01777@naver.com>
2025-06-09 14:22:23 +0900

    refactor: 파일 구조 및 스타일 수정

--- src/app/myprofile/layout.tsx
+++ src/app/myprofile/layout.tsx
@@ -1,12 +1,15 @@
-// src/app/myprofile/layout.tsx
 // import Gnb from '@/components/Gnb';
 // import ProfileCard from '@/components/ProfileCard';
 
-export default function MyProfileLayout({ children }: { children: React.ReactNode }) {
+export default function MyProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div>
       {/* <Gnb /> */}
-      <main className="p-4 max-w-xl mx-auto">
+      <main>
         {/* <ProfileCard /> */}
         {children}
       </main>
