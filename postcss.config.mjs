c2c5eda18ae0f0963aa9a1f669c2177d60effb37
YuWonKyu <duck01777@naver.com>
2025-06-11 22:33:54 +0900

    fix: postcss.config.mjs에서 충돌 해결

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-=======
 const postcssConfig = {
   plugins: {
     "@tailwindcss/postcss": {},
@@ -13,4 +6,3 @@ const postcssConfig = {
 };
 
 export default postcssConfig;
->>>>>>> c1681c51d4d4b24c8b7f0f9365654d1d4ea2e210
