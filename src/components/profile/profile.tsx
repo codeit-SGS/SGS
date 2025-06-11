4baa46270904c16c80f1facab5ebc5aa8c2b24b8
wnsdn00 <114475584+wnsdn00@users.noreply.github.com>
2025-06-11 12:44:32 +0900

    Update profile.tsx
    작업자가 profile 사이즈 변경 가능하도록 props 추가

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -3,20 +3,25 @@
 import { useState } from "react";
 import Image from "next/image";
 
-export default function ProfileIcon() {
+interface ProfileIconProps {
+  className?: string;
+}
+
+export default function ProfileIcon({ className = "w-[45px] h-[45px]" }: ProfileIconProps) {
   const [isHovering, setIsHovering] = useState(false);
 
   return (
     <div
-      className="w-45 h-45 cursor-pointer"
+      className={`${className} relative cursor-pointer`}
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
       <Image
         src={isHovering ? "/img/profile-hover.svg" : "/img/profile-default.svg"}
         alt="Profile Icon"
-        width={174}
-        height={174}
+        fill
+        sizes="100%"
+        className="object-contain"
       />
     </div>
   );
