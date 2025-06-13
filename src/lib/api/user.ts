ebff3cd06bea44e7dc0c1e8d568e41726db76ec3
YuWonKyu <duck01777@naver.com>
2025-06-14 02:39:08 +0900

    docs: 사용자 프로필 API 설명 주석 추가

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -4,6 +4,11 @@ export const fetchMyProfile = async () => {
   const res = await api.get("users/me");
   return res.data;
 };
+/*
+users/me 엔드포인트로 GET 요청을 보냅니다.
+서버에서 내 프로필 정보를 받아옵니다.
+응답 객체(res)에서 실제 데이터(res.data)만 반환합니다.
+*/
 
 export const patchMyProfile = async ({
   nickname,
@@ -15,3 +20,9 @@ export const patchMyProfile = async ({
   const res = await api.patch("users/me", { nickname, image });
   return res.data;
 };
+/*
+users/me 엔드포인트로 PATCH 요청을 보냅니다.
+요청 본문에 { nickname, image }를 담아 보냅니다.
+서버에서 내 프로필(닉네임, 이미지)을 수정합니다.
+응답 객체(res)에서 실제 데이터(res.data)만 반환합니다.
+ */
