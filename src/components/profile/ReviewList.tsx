5070dddcd800d67079f72911f445549e9be1146c
YuWonKyu <duck01777@naver.com>
2025-06-16 14:49:54 +0900

    feat: 임시 UI

--- src/components/profile/ReviewList.tsx
+++ src/components/profile/ReviewList.tsx
@@ -1,2 +1,6 @@
-const ReviewList = () => <div>ReviewList 임시 컴포넌트</div>;
+const ReviewList = () => (
+  <div className="w-343 h-635 tablet:w-704 tablet:h-655 pc:w-800 pc:h-1086 ">
+    ReviewList 임시 컴포넌트
+  </div>
+);
 export default ReviewList;
--- src/components/profile/WineList.tsx
+++ src/components/profile/WineList.tsx
@@ -1,2 +1,6 @@
-const WineList = () => <div>WineList 임시 컴포넌트</div>;
+const WineList = () => (
+  <div className="w-343 h-635 tablet:w-704 tablet:h-655 pc:w-800 pc:h-1086 ">
+    WineList 임시 컴포넌트
+  </div>
+);
 export default WineList;
