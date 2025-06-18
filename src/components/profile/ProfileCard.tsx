41fd32295af8fb7d2b0f11b86c8565cb2da8ed25
유원규 <duck01777@naver.com>
2025-06-18 11:26:49 +0900

    feat: MyProfileTabs 및 ProfileCard의 스타일 수정으로 레이아웃 개선

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -78,7 +78,7 @@ setIsFirstFocus(false);
 };
 
 return (
-<div className=" pc:max-w-280 tablet:max-w-704 w-full h-241 tablet:h-247 pc:h-530 p-20 tablet:px-40 tablet:py-23 pc:px-20 pc:py-28 rounded-[16px] border border-gray-300 shadow-subtle bg-white">
+<div className=" pc:max-w-280 tablet:w-704 min-w-343 h-241 tablet:h-247 pc:h-530 p-20 tablet:px-40 tablet:py-23 pc:px-20 pc:py-28 rounded-[16px] border border-gray-300 shadow-subtle bg-white">
 <div className="flex flex-col pc:items-center items-start gap-20 tablet:gap-30 pc:gap-48">
 {/* 프로필 사진과 닉네임 표시 영역 */}
 <div className="flex items-center pc:flex-col gap-16 tablet:gap-32">
