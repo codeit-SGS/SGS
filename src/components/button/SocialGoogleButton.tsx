110f096a48381e12441fa978f19716dd06543523
유원규 <duck01777@naver.com>
2025-06-19 22:16:44 +0900

    feat: 소셜 로그인 버튼 컴포넌트 추가

--- src/components/button/SocialGoogleButton.tsx
+++ src/components/button/SocialGoogleButton.tsx
@@ -1,14 +1,27 @@
-import CommonButton from "./CommonButton";
 import Image from "next/image";
 
-const SocialGoogleButton = ({
-  className = "",
-  ...props
-}: React.ComponentProps<"button"> & { className?: string }) => {
+interface SocialGoogleButtonProps {
+  href: string;
+  className?: string;
+}
+
+const SocialGoogleButton = ({ href, className = "" }: SocialGoogleButtonProps) => {
   return (
-    <CommonButton
-      variant="auth-social-google"
-      icon={
+    <a
+      href={href}
+      className={`w-303 tablet:400 h-48 tablet:h-52 rounded-[12px] tablet:rounded-[16px] tablet:text-lg tablet:leading-16  bg-white border border-gray-300 text-gray-800 font-medium leading-14 text-md flex items-center justify-center gap-10 tablet:gap-12 ${className}`}
+    >
+      {/* 모바일: 20px, 테블릿 이상: 24px */}
+      <span className="block tablet:hidden">
+        <Image
+          src="/icon/google.svg"
+          alt="구글 아이콘"
+          width={20}
+          height={20}
+          priority
+        />
+      </span>
+      <span className="hidden tablet:block">
         <Image
           src="/icon/google.svg"
           alt="구글 아이콘"
@@ -16,12 +29,9 @@ const SocialGoogleButton = ({
           height={24}
           priority
         />
-      }
-      className={className}
-      {...props}
-    >
+      </span>
       Google로 시작하기
-    </CommonButton>
+    </a>
   );
 };
 
