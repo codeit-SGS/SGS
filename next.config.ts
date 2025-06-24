ea6e2bd64cb6db12b8c8f9fa805e58c8faa7afcf
DESKTOP-JBD7NHD\User <qhrud0130@gmail.com>
2025-06-24 16:38:58 +0900

    fix : 배포용 eslint 설정 무시

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드(배포) 시 ESLint 오류 무시
   },
 };
 
