3a2b3b94ab58cf46c548cd1764d19446a75c31db
DESKTOP-JBD7NHD\User <qhrud0130@gmail.com>
2025-06-24 16:43:30 +0900

    fix : next.config.ts -> .js로 수정

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  images: {
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
